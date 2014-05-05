0c98d69e266c004b804c8aa01ec5b7b2b1028fd5
mspkvp <msp.univ.por@gmail.com>
2014-05-06 00:21:55 +0100

    .

--- DSL Parser/src/MyVariable.java
+++ DSL Parser/src/MyVariable.java
@@ -1,10 +1,10 @@
-
 public class MyVariable {
 	public static boolean rangeOnDeclaration = false;
 	public int rangeArray;
 	public boolean multiplier;
 	public String symbol;
 	public Token type;
+	public boolean input = true;
 	
 	// for multiplier declaration
 	public MyVariable(String symbol, Token type){
@@ -14,10 +14,13 @@ public class MyVariable {
 	}
 	
 	// for matrix/vector declaration
-	public MyVariable(String symbol, Token type, int rangeArray){
+	public MyVariable(String symbol, Token type, Token input, int rangeArray) throws ParseException{
 		this.symbol = symbol;
 		this.type = type;
+		if( rangeArray != -1 && rangeOnDeclaration ) throw new ParseException("Array range missing");
 		this.rangeArray = rangeArray;
 		multiplier = false;
+		if(input.image.equals("in")) this.input = true;
+		else this.input = false;
 	}
 }
---
+++ DSL Parser/src/VariableStore.java
@@ -0,0 +1,5 @@
+import java.util.Vector;
+
+public class VariableStore {
+	public static Vector<MyVariable> storage = new Vector<MyVariable>();
+}
