ecfda99530561f502194fc59a1836272004e9888
mspkvp <msp.univ.por@gmail.com>
2014-05-13 21:54:26 +0100

    try

--- DSL Parser/src/Generator.java
+++ DSL Parser/src/Generator.java
@@ -64,7 +64,7 @@ public class Generator {
 		}while(!stop);
 		
 		//out.write(generatedCode);
-//		out.write("hello"+String.format("%n"));
+		out.write("hello"+String.format("%n"));
 		
 		stop=false;
 		do{
