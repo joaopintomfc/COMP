e15cd4a75c3ef8bdad5e52f7af721edbf6b71055
mspkvp <msp.univ.por@gmail.com>
2014-05-13 11:17:42 +0100

    Missing: - 2 Semantic error detections - All Warnings - Code Generation - Injection of Generated code to the original .java file

---
+++ DSL Parser/src/Generator.java
@@ -0,0 +1,37 @@
+import java.io.File;
+
+
+public class Generator {
+
+	public Generator(File file, SimpleNode ast){
+		
+	}
+	
+	/**
+	 * 
+	 * @param filename
+	 * outputs to the generated output file all the code before the '@mat' tag
+	 */
+	public void writeTop(String filename){
+		
+	}
+	
+	/**
+	 * 
+	 * @param filename
+	 * @param code
+	 * outputs all the generated code to the final output file
+	 */
+	public void writeGenerated(String filename, String code){
+		
+	}
+	
+	/**
+	 * 
+	 * @param filename
+	 * outputs to the generated output file all the code after the '@mat' tagged block 
+	 */
+	public void writeBottom(String filename){
+		
+	}
+}
