fdee1b867467c4c100e96ff963df5dd6297ac051
luisfilipe46 <ei11059@fe.up.pt>
2014-05-15 20:06:04 +0100

    requere testes. ja suporta range na operacao e declaracao. AST com ranges das operacoes apenas. falta apenas mais teste e fazer producao para copia de codigo antes e dps da tag

--- DSL Parser/src/MyVariable.java
+++ DSL Parser/src/MyVariable.java
@@ -1,50 +1,52 @@
 public class MyVariable {
-	public static boolean rangeOnDeclaration = false;
-	public static int range = -1;
+	// public static boolean rangeOnDeclaration = false;
+	// public static int range = -1;
 	public int rangeArray;
 	public boolean multiplier;
 	public String symbol;
 	public Token type;
 	public boolean input = true;
-	
-	public static void clear(){
-		range = -1;
-		rangeOnDeclaration = false;
+
+	public static void clear() {
+		// range = -1;
+		// rangeOnDeclaration = false;
 	}
-	
+
 	// for multiplier declaration
-	public MyVariable(String symbol, Token type){
+	public MyVariable(String symbol, Token type) {
 		this.symbol = symbol;
 		this.type = type;
 		multiplier = true;
 	}
-	
+
 	// for matrix/vector declaration
-	public MyVariable(String symbol, Token type, Token input, int rangeArray) throws ParseException{
+	public MyVariable(String symbol, Token type, Token input, int rangeArray)
+			throws ParseException {
 		this.symbol = symbol;
 		this.type = type;
-		if(rangeArray > range) range = rangeArray;
+		// if(rangeArray > range) range = rangeArray;
 		this.rangeArray = rangeArray;
-		
-		if(input.image.equals("in")) this.input = true;
-		else{
-			this.rangeArray = range;
+
+		if (input.image.equals("in"))
+			this.input = true;
+		else {
+			//this.rangeArray = range;
 			this.input = false;
 		}
-		
+
 		multiplier = false;
 	}
-	
-	public String toString(){
-		return new String((input? "in":"out") + " | "
-							+ symbol + " | "
-							+ rangeArray + " | "
-							+ type.image);
+
+	public String toString() {
+		return new String((input ? "in" : "out") + " | " + symbol + " | "
+				+ rangeArray + " | " + type.image);
 	}
 
 	public static boolean checkRange(int parseInt) {
 		double nr = Math.sqrt(parseInt);
-		if(nr-(int)nr == 0 && nr != 1) return true;
-		else return false;
+		if (nr - (int) nr == 0 && nr != 1)
+			return true;
+		else
+			return false;
 	}
 }
