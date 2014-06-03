d2518855b057df440735818cbe3aecdf9a51ba2b
Miguel Cunha <miguelfcunha@gmail.com>
2014-06-03 17:26:35 +0100

    Controlling arguments

--- DSL Parser/Novos Ficheiros de Teste/teste1.java
+++ DSL Parser/Novos Ficheiros de Teste/teste1.java
@@ -1,13 +1,25 @@
-/*@mat
-	in int A[16];
-	in int B[];
-	in float C[];
-	out int resultado[];
-	out float resultado2[];
+public Teste1 {
 	
-	resultado = A + B - (A * B);
+	private int[] A, B;
+	private float[] C, resultado;
 	
-	resultado2 = (A * C) - C;
-*/
+	private void loadVariables(){
+		//TODO: code...
+	}
+	
+	public void doOperation(){
+		//TODO: more code...
+		
+		/*@mat
+		in int A[16];
+		in int B[];
+		in float C[];
+		out float resultado[];
 
-/* Tem que dar certo */
+		resultado = A + B - (A * B) - C;
+
+	*/
+		
+	}
+	
+}
